69967fedb61a994064544d1e4b51a566e3c3a3b7
abhinandhari <37014542+abhinandhari@users.noreply.github.com>
2019-02-25 18:57:03 +0530

    didstuff
    didstuff

--- ac.py
+++ ac.py
@@ -2,7 +2,7 @@
 from PIL import Image
 import os
 os.chdir('/home/abhinand/fossgit/amfoss-freshers-test/assets')
+oswald = Image.open('/home/abhinand/fossgit/amfoss-freshers-test/assets/oswald.png').convert('LA')
 oswald.rotate(270).save('corrected.png')
-oswald = Image.open('/home/abhinand/fossgit/amfoss-freshers-test/assets/corrected.png').convert('LA')
 oswald.save('newos.png')
 
